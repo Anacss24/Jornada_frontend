0e09ad1286422d5726f98891d16d06ae45d123b9
ANA CLAUDIA SANTANA DA SILVA <101267392+Anacss24@users.noreply.github.com>
2023-09-27 23:44:45 +0000

    Inserindo component 'Tag'

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,26 @@ import Card from './components/Card/Card'
 function App() {
   const item1 = {
     nome: 'Rick Sanchez',
-    imgagemURL: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+    imagemURL: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    tags: ['Status: Vivo', 'Espécie: Humana']
   }
 
   const item2 = {
     nome: 'Morty Smith',
-    imgagemURL: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+    imagemURL: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+    tags: ['Origem: Terra C-137']
   }
-
   const item3 = {
     nome: 'Summer Smith',
-    imgagemURL: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+    imagemURL: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
 
   const itens = [item1, item2, item3]
 
   return (
     <>
-    {itens.map(function (item){
-      return <Card item={item}/>
+    {itens.map(function (item, index){
+      return <Card item={item} key={`card_${index}`}/>
     })}
 
     </>
