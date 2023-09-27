7fc340921ff04f3013e335b8312e732f4104149b
ANA CLAUDIA SANTANA DA SILVA <101267392+Anacss24@users.noreply.github.com>
2023-09-27 17:16:43 +0000

    Adicionando propriedades ao  componente 'Card'

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  
+  const item1 = {
+    nome: 'Rick Sanchez',
+    imgagemURL: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
 
   return (
     <>
-     <Card />    
+     <Card item={item1} />    
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,15 @@
 import "./Card.css"
 
-export default function Card() {
+export default function Card(props) {
+   const item =props.item;
+
     return  <div className="card">
-       <h2>Rich Sanchez</h2>
+       <h2>{item.nome}</h2>
        <div className="tags">
           <div className="tag">Status: Vivo</div>
           <div className="tag">Espécie: Humana</div>
           <div className="tag">Origem: Terra C-137</div>
         </div>
-       <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
+       <img src={item.imagemURL} alt="" />
        </div>
  }
