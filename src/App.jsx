26c89975aacd32dc4b5b024d77a4fc57639349a6
ANA CLAUDIA SANTANA DA SILVA <101267392+Anacss24@users.noreply.github.com>
2023-09-27 15:33:45 +0000

    Adicionando elemento no card

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   
 
   return (
     <>
-      
-       
-      <div className="card">
-       <h2>Rich Sanchez</h2>
-       <div className='descricao'>
-       <p>Status: Vivo</p>
-       <p>Espécie: Humana</p>
-       <p>Origem: Terra C-137</p>
-       </div>
-       <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
-       </div>
+     <Card />    
     </>
   )
 }
